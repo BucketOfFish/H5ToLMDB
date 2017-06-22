7a40c9d52b9fc7ddfc466c580760ef2ad43c7cb3
Matt Zhang <set.stun@gmail.com>
2017-06-22 06:48:17 -0700

    Bug fixes

--- convertH5ToLMDB.m
+++ convertH5ToLMDB.m
@@ -1,25 +1,33 @@
 % Matlab code
 
 h5Name = 'EC2_blocks_1_8_9_15_76_89_105_CV_HG_align_window_-0.5_to_0.79_file_nobaseline.h5';
-LMDBName = 'testSet';
+LMDBTest = 'ECoG_test';
+LMDBTrain = 'ECoG_train';
+nTest = 2000;
+nTrain = 572;
 
 X = h5read(h5Name, '/Xhigh gamma');
 y = h5read(h5Name, '/y');
-% size(X) -> 258 86 2572
-% size(y) -> 2572 1
+size(X)
+size(y)
 X = permute(X, [1, 2, 4, 3]); % add dimension
 y = y'; % transpose
 
 % Randomly shuffle data order.
-nSamples = size(X, 3);
+nSamples = size(X, 4);
 perm = randperm(nSamples);
-X = X(:,:,perm);
+X = X(:,:,:,perm);
 y = y(perm);
 
 X = single(X); % cast from uint8 to float
 y = int32(y);
 
-if ~exist(LMDBName, 'dir')
-    mkdir(LMDBName);
+if ~exist(LMDBTest, 'dir')
+    mkdir(LMDBTest);
 end
-write_lmdb(LMDBName, X, y);
+write_lmdb(LMDBTest, X(:,:,:,[1:nTest]), y(:,[1:nTest]));
+
+if ~exist(LMDBTrain, 'dir')
+    mkdir(LMDBTrain);
+end
+write_lmdb(LMDBTest, X(:,:,:,[nTest+1:nTest+nTrain]), y(:,[nTest+1:nTest+nTrain]));
