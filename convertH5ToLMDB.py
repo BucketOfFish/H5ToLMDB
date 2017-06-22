fddc2f99a4e6687d2ff12ab7052b7ea22ba4b742
Matt Zhang <set.stun@gmail.com>
2017-06-22 04:28:43 -0700

    Split into training and test

--- convertH5ToLMDB.py
+++ convertH5ToLMDB.py
@@ -3,17 +3,16 @@
 from numpy import *
 import lmdb
 import h5py
-
-#import sys
-#caffe_root = <where your caffe installation is>
-#sys.path.insert(0, caffe_root + 'python');
-
 import caffe
 
-tgt_db = "/home/tn_user/MattWorkspace/ECoGAnalysis/Data/ECoG"
+tgt_db_test = "/home/tn_user/MattWorkspace/ECoGAnalysis/Data/ECoG_test"
+tgt_db_train = "/home/tn_user/MattWorkspace/ECoGAnalysis/Data/ECoG_train"
 src_db = "/home/tn_user/MattWorkspace/ECoGAnalysis/Data/EC2_blocks_1_8_9_15_76_89_105_CV_HG_align_window_-0.5_to_0.79_file_nobaseline.h5"
 
-env = lmdb.open(tgt_db, map_size=10000000000)
+train = lmdb.open(tgt_db_train, map_size=10000000000)
+test = lmdb.open(tgt_db_test, map_size=10000000000)
+nTrain = 2000
+nTest = 572
 
 with h5py.File(src_db,'r') as f:
     # extract data from hdf file
@@ -24,13 +23,23 @@ with h5py.File(src_db,'r') as f:
     ar_label = ar_label.flatten()
     assert len(ar_label) == n # number of labels has to match the number of input images!
     # write data to lmdb
-    for i in range(n):
-        datum = caffe.proto.caffe_pb2.Datum()
-        datum.channels = c
-        datum.height = h
-        datum.width = w
-        datum.data = ar_data[i,:,:].tobytes()
-        datum.label = ar_label[i]
-        str_id = '{:08}'.format(i) # create an 8 digit string id based on the index
-        with env.begin(write=True) as txn:
+    with train.begin(write=True) as txn:
+        for i in range(nTrain):
+            datum = caffe.proto.caffe_pb2.Datum()
+            datum.channels = c
+            datum.height = h
+            datum.width = w
+            datum.data = ar_data[i,:,:].tobytes()
+            datum.label = ar_label[i]
+            str_id = '{:08}'.format(i) # create an 8 digit string id based on the index
+            txn.put(str_id.encode('ascii'), datum.SerializeToString())
+    with test.begin(write=True) as txn:
+        for i in range(nTest):
+            datum = caffe.proto.caffe_pb2.Datum()
+            datum.channels = c
+            datum.height = h
+            datum.width = w
+            datum.data = ar_data[i+nTrain,:,:].tobytes()
+            datum.label = ar_label[i+nTrain]
+            str_id = '{:08}'.format(i) # create an 8 digit string id based on the index
             txn.put(str_id.encode('ascii'), datum.SerializeToString())
